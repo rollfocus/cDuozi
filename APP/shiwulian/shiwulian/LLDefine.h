3132c36be4a89ef9d6ccc47c8a8abede8357486f
rollfocus <273951489@qq.com>
2016-02-26 19:26:14 +0800

    添加APP原型

---
+++ APP/shiwulian/shiwulian/LLDefine.h
@@ -0,0 +1,16 @@
+//
+//  LLDefine.h
+//  shiwulian
+//
+//  Created by lin zoup on 2/26/16.
+//  Copyright © 2016 shiwulian. All rights reserved.
+//
+
+#ifndef LLDefine_h
+#define LLDefine_h
+
+
+#define Screen_Bounds [[UIScreen mainScreen] bounds]
+
+
+#endif /* LLDefine_h */
